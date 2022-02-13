43b00377259351dd4e679ad38b90a1c135ef40c0
Pablo Acland <pablo.acland@estudiantes.utec.edu.uy>
2022-02-12 22:28:59 -0300

    ejercicio_1 terminado

---
+++ src/EJercicio_1.java
@@ -0,0 +1,15 @@
+public class EJercicio_1 {
+    public static void main(String[] args) {
+        int x=20;
+        int y=20;
+        if (x > y) {
+            System.out.println(x+" es mayor que "+y);
+        }else if (y > x) {
+            System.out.println(y+" es mayor que "+x);
+        }else {
+            System.out.println(x+" es igual que "+y);
+        }
+    }
+
+
+}
--- src/Practicatecnicajava.java
+++
@@ -1,7 +0,0 @@
-public class Practicatecnicajava {
-    public static void main(String[] args) {
-
-    }
-
-
-}
