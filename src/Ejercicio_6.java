53508d47a395ac013fef6dff203fdd90e2dfb33d
Pablo Acland <pablo.acland@estudiantes.utec.edu.uy>
2022-02-12 23:30:25 -0300

    Arreglo del ejercicio_5 y ejercicio_6 terminado

--- src/Ejercicio_6.java
+++ src/Ejercicio_6.java
@@ -1,7 +1,7 @@
-public class Ejercicio_5 {
+public class Ejercicio_6 {
     public static void main(String[] args) {
         int x=1;
-        while (x!=101){
+        for (int i=0;i<100;i++){
             if ((x%2)==0) {
                 System.out.println("Numero par "+x);
                 x++;
