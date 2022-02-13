8cb492ebd75df90cf62e9172549bb51ea00cfedb
Pablo Acland <pablo.acland@estudiantes.utec.edu.uy>
2022-02-12 23:24:29 -0300

    ejercicio_5 terminado

---
+++ src/Ejercicio_5.java
@@ -0,0 +1,9 @@
+public class Ejercicio_5 {
+    public static void main(String[] args) {
+        int x=1;
+        while (x!=101){
+            System.out.println(x);
+            x++;
+        }
+    }
+}
